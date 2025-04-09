56aa766d14f1f26b7711b6b633aced0534f8bee2
Harshit Malviya <harshitmalviya127@gmail.com>
2025-04-09 10:06:24 +0530

    register models in billing/admin.py

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# billing/admin.py (or the app where you defined Sale and SaleItem)
+
+from django.contrib import admin
+from .models import Sale, SaleItem
+
+admin.site.register(Sale)
+admin.site.register(SaleItem)
